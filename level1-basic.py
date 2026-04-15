a3b298d2c074b8fb59acdf9522e3d9697c148768
Ahtisham rao <ahtishamrao997@gmail.com>
2026-04-15 00:31:37 -0700

    Create level1-basic.py

---
+++ level1-basic.py
@@ -0,0 +1,10 @@
+# Ahtisham's Python Journey
+# Level 1: Variables
+
+name = "Ahtisham Saeed:"
+semester = 3
+is-learning-ai = True
+
+print("student name:", name)
+print("semester:", semester)
+print("Learning AI?", is-learning-ai)
