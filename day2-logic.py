8ba1459f1270eefa1cae31481c2bf24e688d4431
Ahtisham rao <ahtishamrao997@gmail.com>
2026-04-15 22:48:23 -0700

    Create day2-logic.py

---
+++ day2-logic.py
@@ -0,0 +1,8 @@
+# Day 2: Uer Input Practice 
+name = input("my name is Ahtisham Saeed:")
+print("Assalam-o-Alaikum", + name)
+
+# Age calculation 
+age_input = input("Enter your age:")
+age = int(age_input)
+print("Agle year app " + str(age + 1) + " year ke ho jayen ge!")
